c124f286c282cf168020cdd942175d3e0839364e
Armando Gonzalez <iam@armando.mx>
2015-04-01 22:17:45 -0600

    Added sylon support

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path            = require('path');
 
 var app     = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
