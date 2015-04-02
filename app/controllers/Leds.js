70c7d1eb1d66bda072bdac9faa41f0c97493f002
Armando Gonzalez <iam@armando.mx>
2015-04-01 23:34:08 -0600

    testing with one led

--- app/controllers/Leds.js
+++ app/controllers/Leds.js
@@ -7,22 +7,25 @@ module.exports = function(app) {
     connection: { name: 'raspi', adaptor: 'raspi' },
 
     devices: {
-      blue: { driver: 'led', pin: 11 },
-      green: { driver: 'led', pin: 7 }
+      //blue: { driver: 'led', pin: 11 },
+      //green: { driver: 'led', pin: 7 },
+      led: { driver: 'led', pin: 11 }
     },
 
     work: function (my) {
       app.route('/api/:led/:position').get(function(req, res, next) {
         var led = req.params.led;
 
-        if(req.params.position == 'on') {
+        every((1).second(), my.led.toggle);
+
+        /*if(req.params.position == 'on') {
           if( led == 'blue' )    my.blue.turnOn();
           if( led == 'green' )   my.green.turnOn();
         }
         else {
           if( led == 'blue' )  my.blue.turnOff();
           if( led == 'green' )   my.green.turnOff();
-        }
+        }*/
 
         res.sendStatus(200);
       });
