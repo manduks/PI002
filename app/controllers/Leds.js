753bdb2785bb060df59213e5c48d46c3ea5bbda9
Armando Gonzalez <iam@armando.mx>
2015-04-01 21:59:00 -0600

    support for blue and green led

--- app/controllers/Leds.js
+++ app/controllers/Leds.js
@@ -7,8 +7,7 @@ module.exports = function(app) {
     connection: { name: 'raspi', adaptor: 'raspi' },
 
     devices: [
-      { name: 'red', driver: 'led', pin: 15 },
-      { name: 'yellow', driver: 'led', pin: 11 },
+      { name: 'blue', driver: 'led', pin: 11 },
       { name: 'green', driver: 'led', pin: 7 }
     ],
 
@@ -17,13 +16,11 @@ module.exports = function(app) {
         var led = req.params.led;
 
         if(req.params.position == 'on') {
-          if( led == 'red' )     my.red.turnOn();
-          if( led == 'yellow' )  my.yellow.turnOn();
+          if( led == 'blue' )    my.yellow.turnOn();
           if( led == 'green' )   my.green.turnOn();
         }
         else {
-          if( led == 'red' )     my.red.turnOff();
-          if( led == 'yellow' )  my.yellow.turnOff();
+          if( led == 'blue' )  my.yellow.turnOff();
           if( led == 'green' )   my.green.turnOff();
         }
 
